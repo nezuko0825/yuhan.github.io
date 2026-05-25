4f232f91d3906a9f5411e317004aae0a9d86080b
yanmei0825 <yanmei08252mail.ru>
2026-05-25 06:46:15 +0300

    edit 1

--- gatsby-config-temp.js
+++ gatsby-config-temp.js
@@ -2,9 +2,9 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Zhang Yu Han',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+      'Zhang Yu Han is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
     siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
@@ -21,8 +21,8 @@ module.exports = {
     // {
     //   resolve: `gatsby-plugin-manifest`,
     //   options: {
-    //     name: 'Brittany Chiang',
-    //     short_name: 'Brittany Chiang',
+    //     name: 'Zhang Yu Han',
+    //     short_name: 'Zhang Yu Han',
     //     start_url: '/',
     //     background_color: config.colors.darkNavy,
     //     theme_color: config.colors.navy,
