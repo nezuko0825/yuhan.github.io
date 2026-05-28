af15612c94901f090f81d28bd587b368a13644a1
yanmei0825 <yanmei08252mail.ru>
2026-05-29 00:40:18 +0300

    edit 28

--- src/components/footer.js
+++ src/components/footer.js
@@ -110,14 +110,8 @@ const Footer = () => {
 
           {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
-              <span>
-                <Icon name="Star" />
-                {/* <span>{githubInfo.stars.toLocaleString()}</span> */}
-              </span>
-              <span>
-                <Icon name="Fork" />
-                {/* <span>{githubInfo.forks.toLocaleString()}</span> */}
-              </span>
+              <span>{/* <span>{githubInfo.stars.toLocaleString()}</span> */}</span>
+              <span>{/* <span>{githubInfo.forks.toLocaleString()}</span> */}</span>
             </div>
           )}
         </a>
